b39b3bf5d6201b303d9d270e64d8d32929ad3a17
Alex Berliner <alexberliner@gmail.com>
2020-01-12 21:43:58 -0500

    Iterate through all months in meds

--- src/mainer.py
+++ src/mainer.py
@@ -1,22 +1,14 @@
+from datetime import *
 from dateutil.relativedelta import relativedelta
+from headache_table import *
 import copy
 import csv
-from headache_table import *
+
 sys.path.insert(0, "../utils")
 from time_util import *
-from datetime import *
-mydb = sqlite3.connect('mydb.db')
-# daylio_parser.parse_daylio(mydb, biometrics_context)
-headache_table = HeadacheTable()
-headache_table.create_table(mydb)
-# parse csv entries
-headache_csv = open("data/headache.csv", "r")
-csv_reader = list(csv.reader(headache_csv, delimiter=','))[1:]
-for i in range(len(csv_reader)):
-    row = csv_reader[i]
-    entry = (i,) + (int(row[0]),) + (int(float(row[1])),)
-    headache_table.add_entry(mydb, entry)
+
 def get_month_pct(start_time, end_time):
+    global headache_conn
     start_time_str = str(int(start_time.timestamp()))
     day_count = (end_time-start_time).days
     day_count_final = day_count
@@ -27,7 +19,7 @@ def get_month_pct(start_time, end_time):
         day_end = start_time+timedelta(days=day+1)
         day_start_str = str(int(day_start.timestamp()))
         day_end_str = str(int(day_end.timestamp()))
-        wellnesses = list(mydb.execute("select * from HEADACHE where TIME>=%s and TIME<=%s"%(day_start_str, day_end_str)))
+        wellnesses = list(headache_conn.execute("select * from HEADACHE where TIME>=%s and TIME<=%s"%(day_start_str, day_end_str)))
         if len(wellnesses) is 0:
             day_count_final -= 1
             continue
@@ -49,38 +41,39 @@ def get_month_pct(start_time, end_time):
                            datetime.fromtimestamp(wellnesses[i][1])
             wellness_adj_pct = duration/day_record_duration*wellness_pct
             work_str += "%2.2f/%2.2f*%2.2f(=>%2.2f)+ "%(duration.seconds/60/60,day_record_duration.seconds/60/60,wellness_pct, wellness_adj_pct)
-            # print(wellness_pct)
             wellness_day_sum += wellness_adj_pct
         if under_80:
             days_under_80 += 1
         work_str+="=%2.2f"%(wellness_day_sum)
-        # print(work_str)
         wellness_month += [wellness_day_sum]
     if len(wellness_month) is 0:
         return 0
-    print(days_under_80)
     return sum(wellness_month)/len(wellness_month)
-# for i in range(0,11):
-#     v = i+1
-#     start_time = datetime(year=2019,month=v,day=1)
-#     end_time = start_time+relativedelta(months=1)
-#     print(v, get_month_pct(start_time, end_time))
-# for i in range(15,30):
-#     start_time = datetime(year=2019,month=11,day=i)
-#     end_time = start_time+relativedelta(days=1)
-#     # print("day " + str(i))
-#     print(get_month_pct(start_time, end_time))
-#     print("")
-for i in range(1,12):
-    start_time = datetime(year=2019,month=i,day=1)
-    end_time = start_time+relativedelta(months=1)
-    print("month " + str(i))
-    print(get_month_pct(start_time, end_time))
-    print("")
-# start_time = datetime(year=2019,month=11,day=15)
-# end_time = start_time+relativedelta(days=12)
-# # print("day " + str(i))
-# print(get_month_pct(start_time, end_time))
-# print("")
 
-mydb.close()
+def main():
+    global headache_conn
+    headache_conn = sqlite3.connect('mydb.db')
+
+    headache_table = HeadacheTable()
+    headache_table.create_table(headache_conn)
+
+    # parse csv entries
+    headache_csv = open("data/headache.csv", "r")
+    csv_reader = list(csv.reader(headache_csv, delimiter=','))[1:]
+
+    for i in range(len(csv_reader)):
+        row = csv_reader[i]
+        entry = (i,) + (int(row[0]),) + (int(float(row[1])),)
+        headache_table.add_entry(headache_conn, entry)
+
+    for i in range(1,13):
+        start_time = datetime(year=2019, month=1, day=1) + relativedelta(months=i)
+        end_time = start_time + relativedelta(months=1)
+        print("%s to %s"%(start_time,end_time))
+        print(get_month_pct(start_time, end_time))
+        print("")
+
+    headache_conn.close()
+
+if __name__ == "__main__":
+    main()
--- src/medmainer.py
+++ src/medmainer.py
@@ -1,80 +1,62 @@
 from dateutil.relativedelta import relativedelta
+from medicine_table import *
+from datetime import *
 import copy
 import csv
-from medicine_table import *
+
 sys.path.insert(0, "../utils")
 from time_util import *
-from datetime import *
-mydb = sqlite3.connect('mydb.db')
-# daylio_parser.parse_daylio(mydb, biometrics_context)
-headache_table = MedicineTable()
-headache_table.create_table(mydb)
-# parse csv entries
-headache_csv = open("data/medicine.csv", "r")
-csv_reader = list(csv.reader(headache_csv, delimiter=','))[1:]
-for i in range(len(csv_reader)):
-    row = csv_reader[i]
-    entry = (i,) + (int(row[0]),) + (row[1],)
-    headache_table.add_entry(mydb, entry)
-def get_month_pct(monthnum):
-    start_time = datetime(year=2019,month=monthnum,day=1)
-    start_time_str = str(int(start_time.timestamp()))
-    end_time = start_time+relativedelta(months=1)
-    # print(len(wellnesses))
-    # for entry in wellnesses:
-    #     print(entry[2], epoch_to_ymd_hms(entry[1]))
+
+def get_meds_in_period(start_time, end_time):
+    global med_conn
     day_count = (end_time-start_time).days
-    # print(day_count)
-    wellness_month = []
     med_count = {}
     for day in range(day_count):
-        # daystart = copy.deepcopy(start_time)
-        # print(daystart)
         day_start = (start_time+timedelta(days=day)).replace(hour=8, minute=0,second=0)
         day_end = start_time+timedelta(days=day+1)
         day_start_str = str(int(day_start.timestamp()))
         day_end_str = str(int(day_end.timestamp()))
-        # print(epoch_to_ymd_hms(day_end.timestamp()))
-        wellnesses = list(mydb.execute("select * from MEDICINE where TIME>=%s and TIME<=%s"%(day_start_str, day_end_str)))
+        wellnesses = list(med_conn.execute("select * from MEDICINE where TIME>=%s and TIME<=%s"%(day_start_str, day_end_str)))
         for entry in wellnesses:
-            # print(entry[2])
             if entry[2] not in med_count:
                 med_count[entry[2]] = 0
             med_count[entry[2]] += 1
-        # print(start_time+timedelta(days=day))
-    #     if len(wellnesses) is 0:
-    #         # print("no entries for this day")
-    #         continue
-    #     # print(len(wellnesses))
-    #     first_entry_time = datetime.fromtimestamp(wellnesses[0][1])
-    #     day_record_duration = day_end - first_entry_time
-    #     # print(day_record_duration)
-    #     wellness_day_sum = 0
-    #     for i in range(len(wellnesses)):
-    #         entry = wellnesses[i]
-    #         # print(entry)
-    #         wellness_pct = float(entry[2])
-    #         # print("asd", datetime.fromtimestamp(entry[1]))
-    #         duration = 0
-    #         # print(epoch_to_ymd_hms(wellnesses[i][1]))
-    #         if i == len(wellnesses)-1:
-    #             duration = day_end-datetime.fromtimestamp(wellnesses[i][1])
-    #         else:
-    #             duration = datetime.fromtimestamp(wellnesses[i+1][1])-\
-    #                     datetime.fromtimestamp(wellnesses[i][1])
-    #         wellness_pct = duration/day_record_duration*wellness_pct
-    #         # print("%s/%s*%s="%(duration,day_record_duration,wellness_pct))
-    #         # print(wellness_pct)
-    #         wellness_day_sum += wellness_pct
-    #     wellness_month += [wellness_day_sum]
-    #     # print(epoch_to_ymd_hms(day_start.timestamp()), wellness_day_sum)
-    #         # print("")
-    #     # print("sum %s"%(wellness_day_sum))
-    # # print(wellness_month)
     return med_count
-for i in range(0,11):
-    v = i+1
-    print(v)
-    print(get_month_pct(v))
-    print("")
-mydb.close()
+
+def main():
+    global med_conn
+    med_conn = sqlite3.connect('mydb.db')
+
+    medicine_table = MedicineTable()
+    medicine_table.create_table(med_conn)
+
+    # parse csv entries
+    medicine_csv = open("data/medicine.csv", "r")
+
+    # get all lines but first one, which is a header
+    csv_reader = list(csv.reader(medicine_csv, delimiter=','))[1:]
+
+    first_entry_time = csv_reader[0][0]
+    last_entry_time  = csv_reader[-1][0]
+    for i in range(len(csv_reader)):
+        row = csv_reader[i]
+        entry = (i,) + (int(row[0]),) + (row[1],)
+        medicine_table.add_entry(med_conn, entry)
+
+    first_month_floor_epoch = epoch_month_floor(first_entry_time)
+    last_month_ceil_epoch = epoch_month_floor(last_entry_time)
+    month_delta = datetime_month_delta(first_month_floor_epoch, last_month_ceil_epoch)
+    # e1 = datetime.fromtimestamp(e1)
+    start_time = datetime.fromtimestamp(first_month_floor_epoch)
+
+    for i in range(1,month_delta+1):
+        month_start_time = start_time + relativedelta(months=i)
+        month_end_time = month_start_time + relativedelta(months=1)
+        print("%s to %s" % (month_start_time,month_end_time))
+        print(get_meds_in_period(month_start_time, month_end_time))
+        print("")
+
+    med_conn.close()
+
+if __name__ == "__main__":
+    main()
