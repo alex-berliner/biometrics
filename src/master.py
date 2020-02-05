e51dba4786e44078e3872798b902e728dd9599cc
Alex Berliner <alexberliner@gmail.com>
2020-02-04 20:08:27 -0500

    Give functions namespace until they have their own files

--- src/master.py
+++ src/master.py
@@ -15,34 +15,32 @@ class StdField():
         self.time = val_tuple[1]
         self.val = val_tuple[2]
 
-def get_meds_in_period(conn, start_time, end_time):
-    day_count = (end_time-start_time).days
-    med_count = {}
-    for day in range(day_count):
-        day_start = (start_time+timedelta(days=day)).replace(hour=8, minute=0,second=0)
-        day_end = start_time+timedelta(days=day+1)
-        day_start_str = str(int(day_start.timestamp()))
-        day_end_str = str(int(day_end.timestamp()))
-        wellnesses = list(conn.execute("select * from MEDICINE where TIME>=%s and TIME<=%s"%(day_start_str, day_end_str)))
-        for entry in wellnesses:
-            if entry[2] not in med_count:
-                med_count[entry[2]] = 0
-            med_count[entry[2]] += 1
-    return med_count
+def medicine_add_entries(conn, medicine_csv_handle):
+    medicine_table = MedicineTable()
+    medicine_table.drop_table(conn)
+    medicine_table.create_table(conn)
 
-def print_delta_info(start:timedelta, stop:timedelta, delta:relativedelta):
-    elapsed_runner = start
-    while elapsed_runner < stop and elapsed_runner < datetime.now():
-        elapsed_end = elapsed_runner + delta
-        med_data = get_med_data(conn, elapsed_runner, elapsed_end)
-        print("%2.2f | "%(get_period_pct(conn, elapsed_runner, elapsed_end)), end=" ")
-        print("%s to %s" % (epoch_to_yyyy_mm_dd(elapsed_runner.timestamp()),epoch_to_yyyy_mm_dd(elapsed_end.timestamp())))
-        print(med_data)
-        print("")
-        elapsed_runner += delta
+    # get all lines but first one, which is a header
+    csv_reader = list(csv.reader(medicine_csv_handle, delimiter=','))[1:]
+
+    for i in range(len(csv_reader)):
+        row = csv_reader[i]
+        entry = (i,) + (int(row[0]),) + (row[1],)
+        medicine_table.add_entry(conn, entry)
+
+def medicine_get_data(conn, start_time, end_time):
+    meds = list(conn.execute("select * from medicine where TIME>=%d and TIME<=%d"%(start_time.timestamp(), end_time.timestamp())))
+    med_dict = {}
+    if len(meds) > 0:
+        for med in meds:
+            name = med[2]
+            if name not in med_dict:
+                med_dict[name] = 0
+            med_dict[name] += 1
+    return med_dict
 
 # calculate average of percentages in period
-def get_period_pct(conn, start:timedelta, stop:timedelta):
+def headache_get_period_pct(conn, start:timedelta, stop:timedelta):
     # always calculate period percent based on daily percents
     day_delta = relativedelta(days=1)
 
@@ -81,20 +79,21 @@ def get_period_pct(conn, start:timedelta, stop:timedelta):
 
     return statistics.mean(wellness_days_in_period)
 
-def add_medicine_entries(conn, medicine_csv_handle):
-    medicine_table = MedicineTable()
-    medicine_table.drop_table(conn)
-    medicine_table.create_table(conn)
+def headache_std_dev_calc(conn, start, stop):
+    # always calculate period percent based on daily percents
+    day_delta = relativedelta(days=1)
+    elapsed_runner = start
+    pcts = []
+    while elapsed_runner < stop and elapsed_runner < datetime.now():
+        elapsed_end = elapsed_runner + day_delta
+        period_pct = headache_get_period_pct(conn, elapsed_runner, elapsed_end)
+        if period_pct:
+            pcts += [period_pct]
+        elapsed_runner += day_delta
 
-    # get all lines but first one, which is a header
-    csv_reader = list(csv.reader(medicine_csv_handle, delimiter=','))[1:]
+    return statistics.mean(pcts), statistics.stdev(pcts)
 
-    for i in range(len(csv_reader)):
-        row = csv_reader[i]
-        entry = (i,) + (int(row[0]),) + (row[1],)
-        medicine_table.add_entry(conn, entry)
-
-def add_headache_entries(conn, headache_csv_handle):
+def headache_add_entries(conn, headache_csv_handle):
     headache_table = HeadacheTable()
     headache_table.drop_table(conn)
     headache_table.create_table(conn)
@@ -107,32 +106,7 @@ def add_headache_entries(conn, headache_csv_handle):
         entry = (i,) + (int(row[0]),) + (float(row[1]),)
         headache_table.add_entry(conn, entry)
 
-def get_med_data(conn, start_time, end_time):
-    meds = list(conn.execute("select * from medicine where TIME>=%d and TIME<=%d"%(start_time.timestamp(), end_time.timestamp())))
-    med_dict = {}
-    if len(meds) > 0:
-        for med in meds:
-            name = med[2]
-            if name not in med_dict:
-                med_dict[name] = 0
-            med_dict[name] += 1
-    return med_dict
-
-def std_dev_calc(conn, start, stop):
-    # always calculate period percent based on daily percents
-    day_delta = relativedelta(days=1)
-    elapsed_runner = start
-    pcts = []
-    while elapsed_runner < stop and elapsed_runner < datetime.now():
-        elapsed_end = elapsed_runner + day_delta
-        period_pct = get_period_pct(conn, elapsed_runner, elapsed_end)
-        if period_pct:
-            pcts += [period_pct]
-        elapsed_runner += day_delta
-
-    return statistics.mean(pcts), statistics.stdev(pcts)
-
-def all_stats(conn, start, stop, period):
+def all_get_stats(conn, start, stop, period):
     elapsed_runner = start
     elapsed_arr = []
     while elapsed_runner < min(datetime.now(), stop):
@@ -142,8 +116,8 @@ def all_stats(conn, start, stop, period):
     retstr = ""
     for elapsed in reversed(elapsed_arr):
         elapsed_end = elapsed + period
-        mean, stdev = std_dev_calc(conn, elapsed, elapsed_end)
-        med_data = get_med_data(conn, elapsed, elapsed_end)
+        mean, stdev = headache_std_dev_calc(conn, elapsed, elapsed_end)
+        med_data = medicine_get_data(conn, elapsed, elapsed_end)
         retstr += "%s --- %s"%(str(epoch_to_yyyy_mm_dd(elapsed.timestamp())), str(epoch_to_yyyy_mm_dd(elapsed_end.timestamp()))) + "\n"
         retstr += "mean: %2.2f; stdev: %2.2f"%(mean, stdev) + "\n"
         retstr += "med data: %s"%(str(med_data)) + "\n"
@@ -151,27 +125,29 @@ def all_stats(conn, start, stop, period):
 
     return retstr
 
-
 def main():
     conn = sqlite3.connect('mydb.db')
 
-    # parse csv entries
+    # parse headache csv entries
     headache_csv = open("data/headache.csv", "r")
-    add_headache_entries(conn, headache_csv)
+    headache_add_entries(conn, headache_csv)
 
-    # parse csv entries
+    # parse medicine csv entries
     medicine_csv_handle = open("data/medicine.csv", "r")
-    add_medicine_entries(conn, medicine_csv_handle)
+    medicine_add_entries(conn, medicine_csv_handle)
 
     headache_table_entries = list(conn.execute("""SELECT time FROM headache ORDER BY time ASC"""))
 
+    # time of first entry
     first_entry_time = int(headache_table_entries[0][0])
+
+    # time of last entry
     last_entry_time  = datetime.now().timestamp()
 
     start_time = datetime.fromtimestamp(epoch_month_floor(first_entry_time))
     end_time   = datetime.fromtimestamp(epoch_month_ceil(last_entry_time))
 
-    print(all_stats(conn, start_time, end_time, relativedelta(months=1)).strip())
+    print(all_get_stats(conn, start_time, end_time, relativedelta(months=1)).strip())
     conn.close()
 
 if __name__ == "__main__":
